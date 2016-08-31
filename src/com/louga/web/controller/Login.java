a7fd2dab43840975deeba752f35c6ebd599d95ed
tol <tolbrandir857@gmail.com>
2016-08-31 13:04:53 +0800

    引入bootStrap

---
+++ src/com/louga/web/controller/Login.java
@@ -0,0 +1,23 @@
+package com.louga.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;  
+  
+@Controller  
+public class Login {  
+  
+	@RequestMapping("/login.do")
+    public ModelAndView getAreas(HttpServletRequest request, Model model){  
+          String userName = request.getParameter("userName");
+          String passWord = request.getParameter("passWord");
+          
+          if(userName.equals(passWord)){
+              return new ModelAndView("/main"); 
+          }      
+        return new ModelAndView("/getAreas.do");  
+    } 
+}  
