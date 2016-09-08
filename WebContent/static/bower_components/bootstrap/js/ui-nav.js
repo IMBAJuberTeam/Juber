64dd12749ba4e0759a9fce9a49a6bcd30a18b2aa
tol <tolbrandir857@gmail.com>
2016-09-08 16:56:19 +0800

    分页和表格框架引入

---
+++ WebContent/static/bower_components/bootstrap/js/ui-nav.js
@@ -0,0 +1,22 @@
++function ($) {
+
+  $(function(){
+
+      // nav
+      $(document).on('click', '[ui-nav] a', function (e) {
+//    	  alert(  $("#iframe").attr("src"));
+//    	  $("#iframe").attr("src","/louga/fuck.jsp");
+//    	  $("#iframe").attr("src","/louga/fuck.jsp");
+//          $("#appcontent").html("<iframe id='iframe' src='1.html'></iframe>");
+//          $("#appcontent").show();
+//    	  alert(  $("#iframe").attr("src"));
+//    	  $("#content").append("<jsp:include page='/pages/test/test.jsp'></jsp:include>");
+//    	  window.location.reload();
+//    	  $("#appcontent").load("/pages/test/test.jsp");
+//    	  $("#iframe").src="index.jsp"
+//    	  $("#content").append("<%@ include file='/pages/test/test.jsp' %>");
+//    	  $("#content").append("<iframe src='/louga/pages/test/test.jsp'></iframe>");
+      });
+
+  });
+}(jQuery);
